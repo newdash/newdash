d8d5cefdce148bc6844823933ee1f69f28706c9b
Theo Sun <theo.sun@sap.com>
2020-05-27 15:21:51 +0800

    feat: umd package support

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const { join } = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  output: {
+    library: '_',
+    path: join(__dirname, './dist'),
+    filename: 'newdash.js',
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          configFile: 'tsconfig.umd.json'
+        }
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
+  }
+}
