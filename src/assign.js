aca803e9e57c662a96645149626598a5703ca420
Theo Sun <theo.sun@sap.com>
2020-05-27 13:43:10 +0800

    chore: update

--- src/assign.js
+++ src/assign.js
@@ -1,43 +1,42 @@
+import assignValue from './.internal/assignValue'
+import copyObject from './.internal/copyObject'
 import createAssigner from './.internal/createAssigner'
 import isPrototype from './.internal/isPrototype'
-import copyObject from './.internal/copyObject'
-import assignValue from './.internal/assignValue'
-import keys from './keys'
 import isArrayLike from './isArrayLike'
-
+import keys from './keys'
 
 /**
-    * Assigns own enumerable string keyed properties of source objects to the
-    * destination object. Source objects are applied from left to right.
-    * Subsequent sources overwrite property assignments of previous sources.
-    *
-    * **Note:** This method mutates `object` and is loosely based on
-    * [`Object.assign`](https://mdn.io/Object/assign).
-    *
-    * @static
-    * @memberOf _
-    * @since 0.10.0
-    * @category Object
-    * @param {Object} object The destination object.
-    * @param {...Object} [sources] The source objects.
-    * @returns {Object} Returns `object`.
-    * @see _.assignIn
-    * @example
-    *
-    * function Foo() {
-    *   this.a = 1;
-    * }
-    *
-    * function Bar() {
-    *   this.c = 3;
-    * }
-    *
-    * Foo.prototype.b = 2;
-    * Bar.prototype.d = 4;
-    *
-    * _.assign({ 'a': 0 }, new Foo, new Bar);
-    * // => { 'a': 1, 'c': 3 }
-    */
+  * Assigns own enumerable string keyed properties of source objects to the
+  * destination object. Source objects are applied from left to right.
+  * Subsequent sources overwrite property assignments of previous sources.
+  *
+  * **Note:** This method mutates `object` and is loosely based on
+  * [`Object.assign`](https://mdn.io/Object/assign).
+  *
+  * @static
+  * @memberOf _
+  * @since 0.10.0
+  * @category Object
+  * @param {Object} object The destination object.
+  * @param {...Object} [sources] The source objects.
+  * @returns {Object} Returns `object`.
+  * @see _.assignIn
+  * @example
+  *
+  * function Foo() {
+  *   this.a = 1;
+  * }
+  *
+  * function Bar() {
+  *   this.c = 3;
+  * }
+  *
+  * Foo.prototype.b = 2;
+  * Bar.prototype.d = 4;
+  *
+  * _.assign({ 'a': 0 }, new Foo, new Bar);
+  * // => { 'a': 1, 'c': 3 }
+  */
 const assign = createAssigner((object, source) => {
   if (isPrototype(source) || isArrayLike(source)) {
     copyObject(source, keys(source), object)
