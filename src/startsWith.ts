c28cea1579f6e5c88fa8a34c4103c855e22f138f
Theo Sun <theo.sun@sap.com>
2020-05-29 15:02:59 +0800

    fix: startsWith type

--- src/startsWith.ts
+++ src/startsWith.ts
@@ -1,16 +1,17 @@
 /**
  * Checks if `string` starts with the given target string.
  *
- * @since 3.0.0
+ * @since 5.2.0
  * @category String
- * @param {string} [string=''] The string to inspect.
- * @param {string} [target] The string to search for.
- * @param {number} [position=0] The position to search from.
+ * @param string The string to inspect.
+ * @param target The string to search for.
+ * @param position The position to search from.
  * @returns {boolean} Returns `true` if `string` starts with `target`,
  *  else `false`.
  * @see endsWith, includes
  * @example
  *
+ * ```js
  * startsWith('abc', 'a')
  * // => true
  *
@@ -19,8 +20,9 @@
  *
  * startsWith('abc', 'b', 1)
  * // => true
+ * ```
  */
-function startsWith(string, target, position) {
+function startsWith(string: string = '', target: string, position = 0) {
   const { length } = string;
   position = position == null ? 0 : position;
   if (position < 0) {
