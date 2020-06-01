fcd4c8d0239916a65425f580f198c09afa1c11c6
Theo Sun <theo.sun@sap.com>
2020-06-01 14:07:05 +0800

    docs: memorize type doc

--- src/memoize.ts
+++ src/memoize.ts
@@ -1,19 +1,24 @@
 
-
-interface CacheLike<K = any, V = any> {
-  clear(): void;
-  delete(key: K): boolean;
-  forEach(callbackfn: (value: V, key: K, map: Map<K, V>) => void, thisArg?: any): void;
+interface MapLike<K = any, V = any> {
+  clear?(): void;
+  delete?(key: K): boolean;
+  forEach?(callbackfn: (value: V, key: K, map: Map<K, V>) => void, thisArg?: any): void;
   get(key: K): V | undefined;
   has(key: K): boolean;
   set(key: K, value: V): this;
 }
 
-interface CacheLikeConstructor<K = any, V = any> {
-  new(): CacheLike<K, V>
+interface MapLikeConstructor<K = any, V = any> {
+  new(): MapLike<K, V>
 }
 
-type MemorizedFunction<T = any, K = any, V = any> = T & { cache: CacheLike<K, V> }
+type MemorizedFunction<T extends (...any) => any, K = any, V = any> = {
+  (...args: Parameters<T>): ReturnType<T>;
+  /**
+   * cache instance for this function instance
+   */
+  cache: MapLike<K, V>;
+}
 
 /**
  * Creates a function that memoizes the result of `func`. If `resolver` is
@@ -64,6 +69,7 @@ function memoize<T extends(...any) => any, K>(func: T, resolver?: (...args) => K
   if (typeof func !== 'function' || (resolver != null && typeof resolver !== 'function')) {
     throw new TypeError('Expected a function');
   }
+
   const memoized = function(...args) {
     const key = resolver ? resolver.apply(this, args) : args[0];
     const cache = memoized.cache;
@@ -71,15 +77,19 @@ function memoize<T extends(...any) => any, K>(func: T, resolver?: (...args) => K
     if (cache.has(key)) {
       return cache.get(key);
     }
+
     const result = func.apply(this, args);
     memoized.cache = cache.set(key, result) || cache;
     return result;
   };
+
   memoized.cache = new (memoize.Cache || Map);
-  // @ts-ignore
   return memoized;
 }
 
-memoize['Cache'] = Map as CacheLikeConstructor;
+/**
+ * Cache constructor of memorize function
+ */
+memoize['Cache'] = Map as MapLikeConstructor;
 
 export default memoize;
