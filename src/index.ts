20238d535621d88c57fbd57cd37a44d97636708b
Theo Sun <theo.sun@sap.com>
2020-07-02 14:22:49 +0800

    feat: isEqualWith function

--- src/index.ts
+++ src/index.ts
@@ -93,6 +93,7 @@ import times from './times';
 import reject from './reject';
 import ary from './ary';
 import fromEntries from './fromEntries';
+import isEqualWith from './isEqualWith';
 
 
 export {
@@ -109,7 +110,7 @@ export {
   isNull, isMap, isString, isObject, isNumber, times,
   max, min, maxBy, minBy, slice, replace, trim, keys,
   countBy, differenceBy, groupBy, curry, eq, constant,
-  reject, ary, fromEntries
+  reject, ary, fromEntries, isEqualWith
 };
 
 export default {
@@ -126,6 +127,6 @@ export default {
   isNull, isMap, isString, isObject, isNumber, times,
   max, min, maxBy, minBy, slice, replace, trim, keys,
   countBy, differenceBy, groupBy, curry, eq, constant,
-  reject, ary, fromEntries
+  reject, ary, fromEntries, isEqualWith
 };
 
