b51947f550619e8f57c6756617523db375375604
Theo Sun <theo.sun@outlook.com>
2020-07-05 18:10:27 +0800

    fix: macos test error

--- deno/test/describe.ts
+++ deno/test/describe.ts
@@ -24,4 +24,29 @@ const describe = (suite: string, fn: (...args: any[]) => void) => {
   fn(it)
 }
 
+describe.skip = (suite: string, fn: (...args: any[]) => void) => {
+
+  const it = (name: string, fn: (...args: any[]) => void) => {
+    Deno.test({
+      name: `case '${suite}' - '${name}'`, fn,
+      fn,
+      ignore: true,
+      sanitizeResources: false,
+      sanitizeOps: false,
+    })
+  }
+
+  it.skip = (name: string, fn: (...args: any[]) => void) => {
+    Deno.test({
+      name: `case '${suite}' - '${name}'`, fn,
+      fn,
+      ignore: true,
+      sanitizeResources: false,
+      sanitizeOps: false,
+    })
+  }
+
+  fn(it)
+}
+
 export { describe }
