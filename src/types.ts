8851af2c0e6dc2f0e17612f992bbf46c515a6bd8
Theo Sun <theo.sun@sap.com>
2020-08-24 10:08:02 +0800

    chore(type): deep partial

--- src/types.ts
+++ src/types.ts
@@ -104,3 +104,11 @@ export interface Comparator<T = any> {
  * Unwrap Promise Generic Type
  */
 export type UnwrapPromise<T> = T extends PromiseLike<infer U> ? U : T
+
+
+/**
+ * Same as Partial<T> but goes deeper and makes Partial<T> all its properties and sub-properties.
+ */
+export type DeepPartial<T> = {
+  [P in keyof T]?: T[P] extends Array<infer U> ? Array<DeepPartial<U>> : T[P] extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>> : DeepPartial<T[P]>;
+};
