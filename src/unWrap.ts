fd09e5e78060d1db8f6c1a98583d4a109ea8b66f
Theo Sun <theo.sun@sap.com>
2020-09-22 17:27:01 +0800

    feat: function unWrap

---
+++ src/unWrap.ts
@@ -0,0 +1,23 @@
+import trimPrefix from './trimPrefix';
+import trimSuffix from './trimSuffix';
+
+/**
+ *
+ * unWrap a string
+ *
+ * @since 5.14.0
+ *
+ * @param inner
+ * @param wrapper the wrapped str will be added to inner
+ *
+ *
+ * ```js
+ * unWrap("'value'", "'") => "value"
+ * unWrap("value", "'") => "value"
+ * ```
+ */
+export function unWrap(inner: string, wrapper: string): string {
+  return trimSuffix(trimPrefix(inner, wrapper), wrapper);
+}
+
+export default unWrap;
--- test/wrap.test.js
+++ test/wrap.test.js
@@ -1,12 +1,21 @@
 import assert from 'assert';
+import { unWrap } from '../src/unWrap';
 import { wrap } from '../src/wrap';
+
 
 describe('wrap', () => {
 
   it('should support wrap', () => {
 
-    assert.equal(wrap('aaaaabbcc','e'),'eaaaaabbcce');
-    assert.equal(wrap('aaaaabbcc','a'),'aaaaabbcca');
+    assert.strictEqual(wrap('aaaaabbcc','e'),'eaaaaabbcce');
+    assert.strictEqual(wrap('aaaaabbcc','a'),'aaaaabbcca');
+
+  });
+
+  it('should support unWrap', () => {
+
+    assert.strictEqual(unWrap('aaaaabbcc','e'),'aaaaabbcc');
+    assert.strictEqual(unWrap('aaaaabbcc','a'),'aaaabbcc');
 
   });
 
