23a9c3522dd7f1341d5fd60f7e87dd1737df33cf
Theo Sun <theo.sun@sap.com>
2020-10-14 12:06:36 +0800

    chore: ci test update

--- test/cacheProvider.test.ts
+++ test/cacheProvider.test.ts
@@ -14,15 +14,15 @@ describe2('CacheProviders', () => {
 
   it('should support TTL CacheProvider', async () => {
 
-    const ttlCache = new TTLCacheProvider(500, 2000);
+    const ttlCache = new TTLCacheProvider(500, 1000);
     ttlCache.set('v', 1);
     expect(ttlCache.has('v')).toBeTruthy();
-    await sleep(500);
+    await sleep(510);
     expect(ttlCache.has('v')).toBeFalsy();
     expect(ttlCache.set('v2', '123'));
     await sleep(100);
     expect(ttlCache.has('v2')).toBeTruthy();
-    await sleep(400);
+    await sleep(410);
     expect(ttlCache.has('v2')).toBeFalsy();
     ttlCache.clear();
     expect(ttlCache['timer']).toBeUndefined();
