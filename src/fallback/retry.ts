cadb2032a171f59d9b9b1812f2b39fdc665924a5
Theo Sun <theo.sun@sap.com>
2021-04-28 19:06:08 +0800

    chore(dep): update

--- src/fallback/retry.ts
+++ src/fallback/retry.ts
@@ -7,10 +7,10 @@ import { retry } from '../retry';
  * @since 5.15.0
  * @category Fallback
  * @param runner async function, return promise
- * @param maxRetryCount the maximum number of times a runner should retry, default is 3
+ * @param maxRetryNumber the maximum number of times a runner should retry, default is 3
  * @param retryAfterMSecond the wait milliseconds before retry
  */
-export function fallbackRetry<T>(runner: T, retryTime: number = 3, retryAfterSeconds?: number): T {
+export function fallbackRetry<T>(runner: T, maxRetryNumber: number = 3, retryAfterSeconds?: number): T {
   mustProvide(runner, 'runner', 'function');
-  return retry(runner, retryTime, retryAfterSeconds);
+  return retry(runner, maxRetryNumber, retryAfterSeconds);
 }
